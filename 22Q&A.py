453ff07bb83a0d773d77c1d2f0a2f7e00ae241c9
Namratha <namrathanjjadhav@gmail.com>
2024-07-23 10:12:28 +0530

    Q&A

---
+++ 22Q&A.py
@@ -0,0 +1,27 @@
+#Python program to insert an element at end of an Array.
+numbers = [1,3,5]
+numbers.insert(1,2)
+numbers.insert(1,4)
+print(numbers)
+numbers.insert(10,True)
+print(numbers)
+
+numbers.insert(10,50.0)
+print(numbers)
+
+
+
+#Python program to insert element at a given location in Array.
+input = [1,2,3,4,5]
+element = 6
+index = 2
+insert_at_location(input,element,index)
+print("updated input",input)
+
+#Write a python program  to check teh sizes of givrn teo list are same.
+#Python program to create a list. (all posible ways)
+#Python program to access a given element from the nested list ? 
+#Write a python program to find largest and smallest  number in an list.
+#Write a python program  to find  sum of list elements ?
+#Python program to find heighest frequency elemnt in list.
+#Python program to reverse an list in two ways ?
